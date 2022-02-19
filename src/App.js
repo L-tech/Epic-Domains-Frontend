b0e490ae38df23ce4c548235d3395abc1cccb124
L-tech <legendabrahamonoja@gmail.com>
2022-02-19 20:49:48 +0100

    update: add feature to switch network to matic

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { ethers } from "ethers";
 import contractABI from './utils/contractABI.json';
 import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
+import polygonLogo from './assets/polygonlogo.png';
+import ethLogo from './assets/ethlogo.png';
+import { networks } from './utils/networks';
 
 // Constants
 const TWITTER_HANDLE = 'arome_dev';
@@ -16,6 +19,7 @@ const App = () => {
 	const [currentAccount, setCurrentAccount] = useState('');
 	const [domain, setDomain] = useState('');
   	const [record, setRecord] = useState('');
+	const [network, setNetwork] = useState('');
 
 	const connectWallet = async () => {
 		try {
@@ -56,7 +60,56 @@ const App = () => {
 		} else {
 			console.log('No authorized account found');
 		}
+		const chainId = await ethereum.request({ method: 'eth_chainId' });
+		setNetwork(networks[chainId]);
+
+		ethereum.on('chainChanged', handleChainChanged);
+		function handleChainChanged(_chainId) {
+			window.location.reload();
+		}
 	}
+
+	const switchNetwork = async () => {
+		if (window.ethereum) {
+			try {
+				// Try to switch to the Mumbai testnet
+				await window.ethereum.request({
+					method: 'wallet_switchEthereumChain',
+					params: [{ chainId: '0x13881' }], // Check networks.js for hexadecimal network ids
+				});
+			} catch (error) {
+				// This error code means that the chain we want has not been added to MetaMask
+				// In this case we ask the user to add it to their MetaMask
+				if (error.code === 4902) {
+					try {
+						await window.ethereum.request({
+							method: 'wallet_addEthereumChain',
+							params: [
+								{	
+									chainId: '0x13881',
+									chainName: 'Polygon Mumbai Testnet',
+									rpcUrls: ['https://rpc-mumbai.maticvigil.com/'],
+									nativeCurrency: {
+											name: "Mumbai Matic",
+											symbol: "MATIC",
+											decimals: 18
+									},
+									blockExplorerUrls: ["https://mumbai.polygonscan.com/"]
+								},
+							],
+						});
+					} catch (error) {
+						console.log(error);
+					}
+				}
+				console.log(error);
+			}
+		} else {
+			// If window.ethereum is not found then MetaMask is not installed
+			alert('MetaMask is not installed. Please install it to use this app: https://metamask.io/download.html');
+		} 
+	}
+
 	const mintDomain = async () => {
 		if (!domain) { return }
 		if (domain.length < 3) {
@@ -72,26 +125,26 @@ const App = () => {
 		  const signer = provider.getSigner();
 		  const contract = new ethers.Contract(CONTRACT_ADDRESS, contractABI.abi, signer);
 	
-				console.log("Going to pop wallet now to pay gas...")
-		  let tx = await contract.registerName(domain, {value: ethers.utils.parseEther(price)});
-				const receipt = await tx.wait();
+			console.log("Going to pop wallet now to pay gas...")
+		  	let tx = await contract.registerName(domain, {value: ethers.utils.parseEther(price)});
+			const receipt = await tx.wait();
 	
-				if (receipt.status === 1) {
-					console.log("Domain minted! https://mumbai.polygonscan.com/tx/"+tx.hash);
-					
-					// Set the record for the domain
-					tx = contract.setRecord(domain, record);
-					await tx.wait();
-	
-					console.log("Record set! https://mumbai.polygonscan.com/tx/"+tx.hash);
-					
-					setRecord('');
-					setDomain('');
-				}
-				else {
-					alert("Transaction failed! Please try again");
-				}
-		}
+			if (receipt.status === 1) {
+				console.log("Domain minted! https://mumbai.polygonscan.com/tx/"+tx.hash);
+				
+				// Set the record for the domain
+				let tx1 = contract.setRecord(domain, record);
+				await tx1.wait();
+
+				console.log("Record set! https://mumbai.polygonscan.com/tx/"+tx1.hash);
+				
+				setRecord('');
+				setDomain('');
+			}
+			else {
+				alert("Transaction failed! Please try again");
+			}
+	}
 	  }
 	  catch(error){
 		console.log(error);
@@ -107,6 +160,15 @@ const App = () => {
 		</div>
   	);
 	  const renderInputForm = () =>{
+		if (network !== 'Polygon Mumbai Testnet') {
+			return (
+				<div className="connect-wallet-container">
+					<h2>Please switch to Polygon Mumbai Testnet</h2>
+					{/* This button will call our switch network function */}
+					<button className='cta-button mint-button' onClick={switchNetwork}>Click here to switch</button>
+				</div>
+			);
+		}
 		return (
 			<div className="form-container">
 				<div className="first-row">
@@ -150,8 +212,12 @@ const App = () => {
 				<div className="header-container">
 					<header>
 						<div className="left">
-						<p className="title">😂🤪😹Meme Name Service</p>
-						<p className="subtitle">Your fuuny meme API on the blockchain!</p>
+							<p className="title">😂🤪😹Meme Name Service</p>
+							<p className="subtitle">Your fuuny meme API on the blockchain!</p>
+						</div>
+						<div className="right">
+							<img alt="Network logo" className="logo" src={ network.includes("Polygon") ? polygonLogo : ethLogo} />
+							{ currentAccount ? <p> Wallet: {currentAccount.slice(0, 6)}...{currentAccount.slice(-4)} </p> : <p> Not connected </p> }
 						</div>
 					</header>
 				</div>
